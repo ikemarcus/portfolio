af8602b4e953c20c9c7b3bc6d42b2c2ab19d3606
ikemarcus <i.marcus@sintlucasedu.nl>
2021-12-09 14:48:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,44 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win() {
+    userScore++;
+}
+
+function lose() {
+    
+}
+
+function draw() {
+    console.log("draw");
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+          win();
+          break;
+        case "rp":
+        case "ps":
+        case "sr":
+          lose();
+          break;
+        case "rr":
+        case "pp":
+        case "ss": 
+          draw();
+          break;
+    }
+}
 
 function main() {
     rock_div.addEventListener('click', function() {
@@ -22,3 +60,5 @@ function main() {
         game("s");
     })
 }
+
+main();
