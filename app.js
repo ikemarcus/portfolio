3af1d4990ca784fe518f8b9f6111d4ea4e70071a
ikemarcus <i.marcus@sintlucasedu.nl>
2021-12-09 15:58:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -39,7 +39,12 @@ function lose() {
 }
 
 function draw() {
-    
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computeScore;
+    const smallUserWord = "user".fontsize(3).sub();
+    const smallCompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
 }
 
 function game(userChoice) {
