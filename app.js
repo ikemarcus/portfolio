5ee4567b469f968525e72700fcf3ac478a43a25a
ikemarcus <i.marcus@sintlucasedu.nl>
2021-12-09 15:37:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let computeScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("comp-score");
 const scroreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
@@ -14,17 +14,19 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
-function win() {
+function win(userChoice, computerChoice) {
     userScore++;
-
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computeScore;
+    result_p.innerHTML = userChoice + " beats " + computerChoice + ". You win!";
 }
 
 function lose() {
-    
+
 }
 
 function draw() {
-    console.log("draw");
+    
 }
 
 function game(userChoice) {
@@ -33,17 +35,17 @@ function game(userChoice) {
         case "rs":
         case "pr":
         case "sp":
-          win();
+          win(userChoice, computerChoice);
           break;
         case "rp":
         case "ps":
         case "sr":
-          lose();
+          lose(userChoice, computerChoice);
           break;
         case "rr":
         case "pp":
         case "ss": 
-          draw();
+          draw(userChoice, computerChoice);
           break;
     }
 }
